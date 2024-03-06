7e6c62ccda9371011f34a6adc0a374e254bde265
Moises Alvarenga <moises.alvarenga@outlook.com>
2024-03-05 22:55:51 -0300

    Finalizado projeto ChatPDF com LLM

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 import streamlit as st
+from streamlit_chat import message
 from utils import chatbot, text
 
 def main():
 
     st.set_page_config(page_title='CHatPDF', page_icon=':books:')
+
+    st.header('Converse com seus arquivos')
+    user_question = st.text_input("Faça uma pergunta para mim!")
+
+    if ('conversation' not in st.session_state):
+        st.session_state.conversation = None
+
+
+    if(user_question):
+        response = st.session_state.conversation(user_question)['chat_history']
+
+        for i, text_message in enumerate(response):
+            if(i % 2 == 0):
+                message(text_message.content, is_user = True, key=str(i) + '_user')
+            else:
+                message(text_message.content, is_user=False, key=str(i) + '_bot')
 
     with st.sidebar:
         st.subheader('Seus arquivos')
@@ -15,6 +32,8 @@ def main():
             chunks = text.create_text_chunks(all_files_text)
             
             vectorstore = chatbot.create_vectorstore(chunks)
+
+            st.session_state.conversation = chatbot.create_conversation_chain(vectorstore)
 
 
 
--- utils/chatbot.py
+++ utils/chatbot.py
@@ -1,34 +1,57 @@
-# from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
+# from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain_community.llms import Ollama
-# from dotenv import load_dotenv
-# import os
-# load_dotenv()
+# from langchain_community.llms import Ollama
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-# OpenAi Exxample
-# def create_vectorstore(chunks):
-#     openai_api_key = os.getenv('OPENAI_API_KEY')
-#     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-#     vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
-
-#     return vectorstore
-
-
-#Ollama Example
+# ---------------------------------------- OpenAi Exxample ----------------------------------
 def create_vectorstore(chunks):
-    embeddings = OllamaEmbeddings()
+    openai_api_key = os.getenv('OPENAI_API_KEY')
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
     vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
 
     return vectorstore
 
+
+
+
 def create_conversation_chain(vectorstore):
     llm = ChatOpenAI()
 
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+
+    return conversation_chain
 
 
+# -------------------- Ollama Example ---------------------------------------
 
-# llm = Ollama(model="llama2")
+
+# def create_vectorstore(chunks):
+#     embeddings = OllamaEmbeddings()
+#     vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+
+#     return vectorstore
+
+# def create_conversation_chain(vectorstore):
+#     llm = Ollama(model="llama2")
+
+#     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+#     conversation_chain = ConversationalRetrievalChain.from_llm(
+#         llm=llm,
+#         retriever=vectorstore.as_retriever(),
+#         memory=memory
+#     )
+
+#     return conversation_chain
